b17aac7ed822a91dca13e9c0546546c9004a350e
Gustavo <gdp2116@columbia.edu>
2020-03-18 19:37:30 -0400

    Kill rubbish

--- examples/gen_figure.jl
+++ examples/gen_figure.jl
@@ -6,12 +6,8 @@ k1 = [2.3, 2.8, 3., 4.4, 5.2]
 v1 = [4.1, 1.8, 5., 4.2, 2.]
 
 k2 = [2.3, 2.6, 3.8, 4.1, 4.8, 5.4]
-# k2 = [0.5, 1.0, 1.9, 2.5]
 v2 = [5.0, 0., 9.5, 3., 4.2, -2.]
 
-
-pl1 = PiecewiseLinear(copy(k1), copy(v1))
-pl2 = PiecewiseLinear(copy(k2), copy(v2))
 
 # Plot example
 plot(k1, v1, marker=(:black), line=(:black), label="Function 1", size=(580, 325), dpi=140)
