2fd6e51efb55ecd912deabbdccd24dc8ffbc2ee6
Gustavo <gdp2116@columbia.edu>
2020-03-24 13:56:06 -0400

    Make figure generation random

--- examples/gen_figure.jl
+++ examples/gen_figure.jl
@@ -1,12 +1,18 @@
 using UpperEnvelope
 using Plots
+import Random
 
-# Example
-k1 = [2.3, 2.8, 3., 4.4, 5.2]
-v1 = [4.1, 0.5, 5., 4.2, 2.]
+Random.seed!(42)
 
-k2 = [2.3, 2.6, 3.8, 4.1, 4.8, 5.4]
-v2 = [5.0, 0., 9.5, 3., 4.2, -2.]
+tmp1 = sortslices(reshape(rand(10), (5, 2)), dims=1)
+k1=tmp1[:, 1]
+v1=tmp1[:, 2]
+
+tmp2 = sortslices(reshape(rand(10), (5, 2)), dims=1)
+k2=tmp2[:, 1]
+v2=tmp2[:, 2]
+
+
 
 envelope = compute_envelope((k1, v1), (k2, v2), false)
 
@@ -18,4 +24,4 @@ plot!(p1, k2, v2, marker=(:x, :darkblue), line=(:dash, :darkblue), label="Functi
 plot!(p1,envelope[1], envelope[2], line=(:purple, 3), marker=(:diamond, :purple, 3), label="Upper envelope")
 plot(p0, p1, size=0.8.*(980, 440), dpi=190)
 
-savefig("example.png")
+# savefig("example.png")
