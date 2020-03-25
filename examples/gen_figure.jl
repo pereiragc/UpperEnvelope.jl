20c50106b3a03a64583719496d45f3650c845671
Gustavo <gdp2116@columbia.edu>
2020-03-25 13:44:03 -0400

    Giev up on the subordinate function example for the time being

--- examples/gen_figure.jl
+++ examples/gen_figure.jl
@@ -18,10 +18,10 @@ w2=1 .+ tmp2[:, 3]
 envelope=compute_envelope((k1, v1, w1), (k2, v2, w2), true)
 
 # Plot example (too lazy to make code prettier)
-p0=plot(k1, [v1 w1], marker=(:black), line=(:black), label="Function 1")
-p1=plot(k1, [v1 w1], marker=(:black), line=(:black), label="Function 1")
-plot!(p0, k2, [v2 w2], marker=(:x, :darkblue), line=(:dash, :darkblue), label="Function 2")
-plot!(p1, k2, [v2 w2], marker=(:x, :darkblue), line=(:dash, :darkblue), label="Function 2")
+p0=plot(k1, v1, marker=(:black), line=(:black), label="Function 1")
+p1=plot(k1, v1, marker=(:black), line=(:black), label="Function 1")
+plot!(p0, k2, v2, marker=(:x, :darkblue), line=(:dash, :darkblue), label="Function 2")
+plot!(p1, k2, v2, marker=(:x, :darkblue), line=(:dash, :darkblue), label="Function 2")
 plot!(p1,envelope[1], [envelope[2] envelope[3]], line=(:purple, 3), marker=(:diamond, :purple, 3), label="Upper envelope")
 plot(p0, p1, size=0.8.*(980, 440), dpi=190)
 
